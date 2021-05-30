7777d4b2d564a11fa438dffeecee3d60aa280a23
Bibhash Dash <63647219+bibhashdash@users.noreply.github.com>
2021-05-30 20:03:35 +0100

    added task finished btn and functionality.

--- script.js
+++ script.js
@@ -8,17 +8,25 @@ form.addEventListener("submit", function(event) {
         taskSlot = document.createElement("div");
         taskSlot.classList.add("task-slot");
         taskSlot.innerHTML = `<p class="task-text">${inputFieldValue}</p>
+        <div class="btn-finished"><i class="far fa-check-square"></i></i></div>
     <div class="btn-delete"><i class="far fa-trash-alt"></i></div>`;
 
         container.appendChild(taskSlot);
         form.reset();
         // when task is clicked, turn it green and put a line-through
-        const taskList = document.querySelectorAll(".task-text");
-        for (let i = 0; i < taskList.length; i++) {
-            taskList[i].addEventListener("click", function() {
-                this.classList.add("task-finished");
+
+        const taskFinishedbtns = document.querySelectorAll(".btn-finished");
+        for (let l = 0; l < taskFinishedbtns.length; l++) {
+            taskFinishedbtns[l].addEventListener("click", function() {
+                this.parentElement.classList.add("task-finished");
             });
         }
+        // const taskList = document.querySelectorAll(".task-text");
+        // for (let i = 0; i < taskList.length; i++) {
+        //     taskList[i].addEventListener("click", function() {
+        //         this.classList.add("task-finished");
+        //     });
+        // }
 
         // when delete button is clicked, remove the task
         const deleteBtns = document.querySelectorAll(".btn-delete");
